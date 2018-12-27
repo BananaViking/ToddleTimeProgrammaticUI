3922e5d74fd8048939c74b27ff48bd7f94202004
Banana Viking <bananavikingcoder@gmail.com>
2018-12-26 20:34:36 -0600

    in FarmVC added buttons, stack views, setupButton(), and setupHorizontalStackView()

--- TTProgrammaticUI/Controller/FarmViewController.swift
+++ TTProgrammaticUI/Controller/FarmViewController.swift
@@ -9,12 +9,80 @@
 import UIKit
 
 class FarmViewController: UIViewController {
-
+    let button0 = UIButton()
+    let button1 = UIButton()
+    let button2 = UIButton()
+    let button3 = UIButton()
+    let button4 = UIButton()
+    let button5 = UIButton()
+    
+    let topStackView = UIStackView()
+    let middleStackView = UIStackView()
+    let bottomStackView = UIStackView()
+    let allStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = #colorLiteral(red: 0.1921568627, green: 0.6392156863, blue: 0.2549019608, alpha: 1)
+        
+        let buttons = [button0, button1, button2, button3, button4, button5]
+        
+        for button in buttons {
+            setupButton(button: button)
+        }
+        
+        let horizontalStackViews = [topStackView, middleStackView, bottomStackView]
+        
+        for stackView in horizontalStackViews {
+            setupHorizontalStackView(stackView: stackView)
+        }
+        
+        topStackView.addArrangedSubview(button0)
+        topStackView.addArrangedSubview(button1)
+        
+        middleStackView.addArrangedSubview(button2)
+        middleStackView.addArrangedSubview(button3)
+        
+        bottomStackView.addArrangedSubview(button4)
+        bottomStackView.addArrangedSubview(button5)
+        
+        // allStackView
+        allStackView.axis = .vertical
+        allStackView.distribution = .fillEqually
+        allStackView.spacing = 30
+        allStackView.addArrangedSubview(topStackView)
+        allStackView.addArrangedSubview(middleStackView)
+        allStackView.addArrangedSubview(bottomStackView)
+        allStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(allStackView)
+        
+        // allStackView Constraints
+        allStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        allStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        allStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        allStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        allStackView.topAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        allStackView.bottomAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -79).isActive = true
     }
-
-
+    
+    func setupButton(button: UIView) {
+        button.backgroundColor = #colorLiteral(red: 1, green: 0.8235294118, blue: 0.01176470588, alpha: 1)
+        button.widthAnchor.constraint(greaterThanOrEqualToConstant: 115).isActive = true
+        button.heightAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 4.5, height: 4.5)
+        button.layer.shadowRadius = 4.5
+        button.layer.shadowOpacity = 1
+    }
+    
+    func setupHorizontalStackView(stackView: UIStackView) {
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        allStackView.addArrangedSubview(stackView)
+    }
 }
 
